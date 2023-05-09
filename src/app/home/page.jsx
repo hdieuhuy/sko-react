00250a135c1ae2c60cb11c6f62d166b4ec399fb3
dieuhuy227 <hdieuhuy227@gmail.com>
2023-05-09 07:13:39 +0700

    inital

---
+++ src/app/home/page.jsx
@@ -0,0 +1,13 @@
+import { DatePicker } from "antd";
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <>
+      home page
+      <DatePicker />
+    </>
+  );
+};
+
+export default HomePage;
