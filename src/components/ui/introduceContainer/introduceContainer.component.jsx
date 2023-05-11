15c09a9f12a8302f0f28dd45bcb2f6b5bde42539
dieuhuy227 <hdieuhuy227@gmail.com>
2023-05-11 23:09:57 +0700

    showroom add item

--- src/components/ui/introduceContainer/introduceContainer.component.jsx
+++ src/components/ui/introduceContainer/introduceContainer.component.jsx
@@ -1,8 +1,9 @@
 "use client";
 
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { Carousel, Col, Row, Space, Typography } from "antd";
 import "./introduceContainer.scss";
+import { MdChevronLeft, MdChevronRight } from "react-icons/md";
 
 const tabs = [
   {
@@ -32,25 +33,33 @@ const tabs = [
 ];
 
 export default function IntroduceContainer() {
+  const carouselRef = useRef(null);
   const [currentTab, setCurrentTab] = useState(tabs[0]);
   const showroomItems = [
     {
       image:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQOuERsquFhNE8M8En4HqvOCPEK_6X-rFzckPDKpvPcuidrcGB3KC4oWzwfo1hOlhRYpxM&usqp=CAU",
+        "https://scontent.fsgn2-4.fna.fbcdn.net/v/t39.30808-6/311448017_657930105949438_8580677079473150633_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=730e14&_nc_ohc=niMcMgUFAeAAX-mwveX&_nc_ht=scontent.fsgn2-4.fna&oh=00_AfCKqWvMi-yh03MepOPtpW4zhrxutte-mB8I8EhIKe_0tw&oe=646217DB",
     },
     {
       image:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQOuERsquFhNE8M8En4HqvOCPEK_6X-rFzckPDKpvPcuidrcGB3KC4oWzwfo1hOlhRYpxM&usqp=CAU",
+        "https://media.vov.vn/sites/default/files/styles/large/public/2022-11/289051646_422655006536247_3802310801952961447_n.jpg",
     },
     {
       image:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQOuERsquFhNE8M8En4HqvOCPEK_6X-rFzckPDKpvPcuidrcGB3KC4oWzwfo1hOlhRYpxM&usqp=CAU",
+        "https://afamilycdn.com/150157425591193600/2021/3/26/dan-cast-rm-han-quoc-1-1616738848852739197545-1616746624700-16167466251581916663123.jpg",
     },
     {
-      image:
-        "https://www.packnet.co.nz/images/448247/pid1868086/PROD1.5kg225x375.jpg",
+      image: "https://media.viez.vn/prod/2022/11/22/image_0ba27208e2.jpeg",
     },
   ];
+
+  const next = () => {
+    carouselRef.current.next();
+  };
+
+  const prev = () => {
+    carouselRef.current.prev();
+  };
 
   return (
     <Row justify="center" className="introduce-container">
@@ -112,17 +121,22 @@ export default function IntroduceContainer() {
               ứng
             </Typography.Text>
 
-            <Carousel slidesToShow={3} slidesToScroll={3}>
-              {/* <Space size={28}> */}
-              {showroomItems.map((item, index) => (
-                <div>
+            <div className="showroom-carousel">
+              <MdChevronLeft onClick={prev} className="icon left" />
+              <Carousel
+                slidesToShow={3}
+                slidesToScroll={3}
+                dots={false}
+                infinite={false}
+                draggable
+                ref={carouselRef}
+              >
+                {showroomItems.map((item, index) => (
                   <img key={`showroom-item-${index}`} src={item.image} />
-                </div>
-                //   abc
-                // </div>
-              ))}
-              {/* </Space> */}
-            </Carousel>
+                ))}
+              </Carousel>
+              <MdChevronRight onClick={next} className="icon right" />
+            </div>
           </Space>
         </Col>
       </Row>
